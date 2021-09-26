31dda52463b938af500949f3f9d1912147593cc2
Jakub Melzacki <melzacki.jakub@gmail.com>
2021-09-26 05:46:31 +0200

    added docstrings to methods in items.py and runes.py

--- src/lol_scrapers/items.py
+++ src/lol_scrapers/items.py
@@ -6,6 +6,48 @@ from requests_html import Element, HTML
 class ItemScraper:
 
     def get_items(self, champion_html: HTML):
+        """Scraps item data from opgg html page abot given champion.
+
+        We re looking for item table in html and if there is no tables
+        that means champion is rip. If we can find tables we divide table
+        for rows with items. Each row we separate for win rate, pick rate
+        and items in row. After get all data we separate them on 3 categories
+        starter items, recommended builds and boots.
+
+        Parameters
+        __________
+        champion_html: HTML
+            The champion html file with items data
+
+        Returns
+        _______
+        dict
+            If champion is not rip(There is not enough
+            data available to display statistics) returns
+            The dictionary with all collected data about most
+            played items on given champion. Like boots, starter items
+            and recommended build.
+
+        Example
+        _______
+        >>> session = HTMLSession()
+        >>> resp = session.get("https://champion_url")
+        >>> ItemScraper().get_items(resp.html)
+        {
+            "starter_items": [{
+                "win_rate": "50.74%",
+                "pick_rate": "24.56%",
+                "times_picked": "1765",
+                "items": [{
+                    "name": "Shurelia's Battlesong"
+                    "src": "https://url_to_item_icon"
+                    }, {...}, ...],
+                }, {...}, {...}],
+            "recommended_builds": [{...}, ...],
+            "boots": [{...}, ...],
+            "rip": True if champion is rip else False
+        }
+        """
 
         items_table = champion_html.find("table.champion-overview__table")
 
@@ -42,7 +84,7 @@ class ItemScraper:
             ]
 
             if items:
-                data = self._prepare_data(items, win_rate, pick_rate, imgs_src)
+                data = self._prepare_items_set(items, win_rate, pick_rate, imgs_src)
                 all_items.append(data)
 
         starter_items, recommended_builds, boots = self._group_items_to_categories(
@@ -58,11 +100,46 @@ class ItemScraper:
 
     @staticmethod
     def _group_items_to_categories(all_items: List[Any]):
+        """Groups all items rows to 3 tuples representing 3 categories.
+
+        Parameters
+        __________
+
+        all_items: List[List[Dict[str, str]]]
+            List of items for each row.
+
+        Returns
+        _______
+
+        Tuple[Tuple[Dict[str, str]]
+            Tuple containing tuple for each 3 categories(starter items, recommended builds, boots)
+        """
+
         starter1, starter2, *recommended, boots1, boots2, boots3 = all_items
         return (starter1, starter2), tuple(recommended), (boots1, boots2, boots3)
 
-    def _prepare_data(
+    def _prepare_items_set(
             self, items: List[str], win_rate: Element, pick_rate: Element, imgs_src: List[str]):
+        """Prepares dict with information about item set from collected data
+
+        Parameters
+        __________
+        items: List[str]
+            List of items names in table row
+        win_rate: Element
+            HTML fragment containing item set win rate
+        pick_rate: Element
+            HTML fragment containing item set pick rate
+        img_src: List[str]
+            List of src for each item in item list
+
+        Returns
+        _______
+        dict
+            Dict of extracted data from parameters and restructured to
+            well arranged dictionary
+        """
+
         win_rate = win_rate.text.strip()
         pick_rate, times_picked = pick_rate.text.strip().split(" ")
 
@@ -82,4 +159,6 @@ class ItemScraper:
 
     @staticmethod
     def src2https(src: str):
+        """Formats src urls to https type
+        """
         return f"https:{src}"
--- src/lol_scrapers/runes.py
+++ src/lol_scrapers/runes.py
@@ -3,6 +3,52 @@ from requests_html import HTML, Element
 
 class RuneScraper:
     def get_runes(self, champion_html: HTML):
+        """Scraps runes data from opgg html page abot given champion.
+
+        We re looking for rune table in html and if there is no tables
+        that means champion is rip. If we can find tables we divide table
+        for main table, title, statistics like pick rate and win rate.
+        From main table we extract active runes
+
+        Parameters
+        __________
+        champion_html: HTML
+            The champion html file with rune data
+
+        Returns
+        _______
+        dict
+            If champion is not rip(There is not enough
+            data available to display statistics) returns
+            The dictionary with all collected data about most
+            played runes on given champion. Like statistics, runes and title.
+
+        Example
+        _______
+        >>> session = HTMLSession()
+        >>> resp = session.get("https://champion_url")
+        >>> RuneScraper().get_runes(resp.html)
+        {
+            "title": "Resolve + Domination,
+            "stats": {
+                "pick_rate": "20,78%",
+                "times_picked": "1353",
+                "win_rate": "50,43%",
+                },
+            "runes": {
+               "main_rune": {
+                    "name": "Guardian",
+                    "src": "https://url_for_main_rune_icon",
+                },
+                "main_tree": [{
+                    "name": "Font of life"
+                    "src": "https://url_for_rune_icon"
+                    }, {...}, ...],
+                "secondary_tree": [{...}, ...]
+            },
+            "rip": True if champion is rip else False
+        }
+        """
         runes_tables = champion_html.find("table.champion-overview__table--rune", first=True)
         rip = False
         if runes_tables is None:
@@ -20,13 +66,25 @@ class RuneScraper:
         runes_table = runes_tables.find("td.champion-overview__data", first=True)
 
         title = runes_title.text.strip()
-        stats = self._format_runes_stats(runes_stats.text)
+        stats = self._extract_runes_statistics(runes_stats.text)
         active_runes = self._extract_runes_from_table(runes_table)
 
         return {"title": title, "stats": stats, "runes": active_runes, "rip": rip}
 
     @staticmethod
-    def _format_runes_stats(runes_stats: str):
+    def _extract_runes_statistics(runes_stats: str):
+        """Extracts statistics from given runes_statistic merged string
+
+        Parameters
+        __________
+        runes_stats: str
+            Merged string from statistics and unneeded words
+
+        Returns
+        _______
+        dict
+            Dict containing extracted data from string
+        """
         _, _, pick_rate, times_picked, _, _, win_rate = runes_stats.strip().split(" ")
         return {
             "pick_rate": pick_rate,
@@ -35,6 +93,18 @@ class RuneScraper:
         }
 
     def _get_runes_from_tree_div(self, tree_div: Element):
+        """Extracts active runes from div
+
+        Parameters
+        __________
+        tree_div: Element
+            Div containing all active runes from selected tree
+
+        Returns
+        _______
+        List[dict]
+            List of all active runes names and their icon urls
+        """
         runes = tree_div.find(
             ".perk-page__item.perk-page__item--active"
         )
@@ -46,6 +116,22 @@ class RuneScraper:
         return runes
 
     def _extract_runes_from_table(self, table: Element):
+        """Extracts all runes from main rune table
+
+        Divides table for two trees and main rune div. For each element we extract
+        needed data like names and icons src
+
+        Parameters
+        __________
+        table: Element
+            HTML table containing all active runes
+
+        Returns
+        _______
+        dict
+            Dict with all extracted data like main rune, icons and
+            all active runes from first and second tree.
+        """
         main_tree_div, secondary_tree_div = table.find(".perk-page")
         main_rune_div = table.find(
             ".perk-page__item.perk-page__item--keystone.perk-page__item--active", first=True
@@ -69,4 +155,6 @@ class RuneScraper:
 
     @staticmethod
     def src2https(src: str):
+        """Formats src urls to https type
+        """
         return f"https:{src}"
